84137a03f8a2d70e9459345a15bb62406e76c67f
Karver91 <o.kurmanov91@gmail.com>
2025-05-30 12:36:55 +0200

    Добавлены тесты для фабрики файл-менеджеров

--- tests/test_payout_report/test_file_manager.py
+++ tests/test_payout_report/test_file_manager.py
@@ -1,26 +1,35 @@
 import os
 from random import choice
 
+import pytest
+
 from repository.file_manager.managers import CSVFileManager, JSONFileManager
+from repository.file_manager.simple_file_manager_factory import FileManagerFactory
 
 
-def test_csv_write(input_data_dict, output_data_path):
-    CSVFileManager(file_path=output_data_path).write_file(data=input_data_dict)
+@pytest.mark.parametrize(
+    "manager",
+    [CSVFileManager, JSONFileManager]
+)
+def test_write(input_data_dict, output_data_path, manager):
+    manager(file_path=output_data_path).write_file(data=input_data_dict)
     assert os.path.exists(output_data_path)
 
 
-def test_csv_read(input_data_paths):
-    __file_path = choice([x for x in input_data_paths if x.endswith("csv")])
-    result = CSVFileManager(file_path=__file_path).read_file()
+@pytest.mark.parametrize(
+    "ext, manager",
+    [
+        ("csv", CSVFileManager),
+        ("json", JSONFileManager)
+    ]
+)
+def test_read(input_data_paths, ext, manager):
+    __file_path = choice([x for x in input_data_paths if x.endswith(ext)])
+    result = manager(file_path=__file_path).read_file()
     assert len(result) > 0
 
 
-def test_json_write(input_data_dict, output_data_path):
-    JSONFileManager(file_path=output_data_path).write_file(data=input_data_dict)
-    assert os.path.exists(output_data_path)
-
-
-def test_json_read(input_data_paths):
-    __file_path = choice([x for x in input_data_paths if x.endswith("json")])
-    result = JSONFileManager(file_path=__file_path).read_file()
-    assert len(result) > 0
+def test_file_manager_factory(ext, manager):
+    file_path = f"data.{ext}"
+    result = FileManagerFactory.create(file_path=file_path)
+    assert result == manager
